470d29076ec36a4658bb1607e4894ef5bba6ce55
Patrick Bucher <patrick.bucher@mailbox.org>
2024-05-29 13:21:58 +0200

    sum up durations

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-use regex::Regex;
 use csv::Reader;
+use regex::Regex;
 use std::collections::HashMap;
 use std::error::Error;
 use std::fs::File;
@@ -31,14 +31,28 @@ pub fn parse_csv(path: String) -> Result<CsvData, Box<dyn Error>> {
 
 pub fn sum_duration(csv: CsvData, col: String) -> Option<CsvData> {
     let pattern = Regex::new("([0-9]+):([0-9]+)").ok()?;
+    let mut total_minutes: usize = 0;
     for row in &csv.rows {
         if !row.contains_key(&col) {
             eprintln!("missing column {col} in row {row:?}");
             return None;
         }
         let val = row.get(&col)?;
-        let caps: Vec<_> = pattern.captures_iter(val).map(|c| c.extract::<2>()).collect();
-        println!("{:?}", caps);
+        let caps: Vec<_> = pattern
+            .captures_iter(val)
+            .map(|c| c.extract::<2>())
+            .map(|(_, hm)| hm)
+            .flatten()
+            .map(|x| x.parse::<usize>())
+            .map(|r| r.map_or(0, |v| v))
+            .collect();
+        let h = caps.get(0)?;
+        let m = caps.get(1)?;
+        let minutes = h * 60 + m;
+        total_minutes += minutes;
     }
+    let total_hours = total_minutes / 60;
+    total_minutes -= total_hours * 60;
+    println!("{total_hours}:{total_minutes}");
     Some(csv)
 }
