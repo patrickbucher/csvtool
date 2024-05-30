ed595d933a7a3a81bda0da199be9e14017fe586e
Patrick Bucher <patrick.bucher@mailbox.org>
2024-05-30 10:03:00 +0200

    refactored and tested duration parser code

--- src/lib.rs
+++ src/lib.rs
@@ -30,16 +30,39 @@ pub fn parse_csv(path: String) -> Result<CsvData, Box<dyn Error>> {
 }
 
 pub fn sum_duration(csv: CsvData, col: String) -> Option<CsvData> {
-    let pattern = Regex::new("([0-9]+):([0-9]+)").ok()?;
-    let mut total_minutes: usize = 0;
+    let parser = DurationParser::new();
+    let mut total_mins: usize = 0;
     for row in &csv.rows {
         if !row.contains_key(&col) {
             eprintln!("missing column {col} in row {row:?}");
             return None;
         }
-        let val = row.get(&col)?;
-        let caps: Vec<_> = pattern
-            .captures_iter(val)
+        let raw = row.get(&col)?;
+        if let Some((h, m)) = parser.parse_duration(raw) {
+            total_mins += m + h * 60;
+        }
+    }
+    let h = total_mins / 60;
+    let m = total_mins - h * 60;
+    println!("{h}:{m}");
+    Some(csv)
+}
+
+struct DurationParser {
+    pattern: Regex,
+}
+
+impl DurationParser {
+    fn new() -> Self {
+        let pattern = "([0-9]+):([0-9]+)";
+        let pattern = Regex::new(pattern).expect(&format!("invalid pattern: '{pattern}'"));
+        DurationParser { pattern }
+    }
+
+    fn parse_duration(&self, raw: &str) -> Option<(usize, usize)> {
+        let caps: Vec<_> = self
+            .pattern
+            .captures_iter(raw)
             .map(|c| c.extract::<2>())
             .map(|(_, hm)| hm)
             .flatten()
@@ -48,11 +71,29 @@ pub fn sum_duration(csv: CsvData, col: String) -> Option<CsvData> {
             .collect();
         let h = caps.get(0)?;
         let m = caps.get(1)?;
-        let minutes = h * 60 + m;
-        total_minutes += minutes;
+        Some((*h, *m))
     }
-    let total_hours = total_minutes / 60;
-    total_minutes -= total_hours * 60;
-    println!("{total_hours}:{total_minutes}");
-    Some(csv)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_duration_parser() {
+        let tests = [
+            ("0:00", Some((0, 0))),
+            ("1:00", Some((1, 0))),
+            ("0:30", Some((0, 30))),
+            ("0:0", Some((0, 0))),
+            ("1.23", None),
+            ("0.00", None),
+        ];
+        let parser = DurationParser::new();
+
+        for (input, expected) in tests {
+            let actual = parser.parse_duration(input);
+            assert_eq!(actual, expected);
+        }
+    }
 }
